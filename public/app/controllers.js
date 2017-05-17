65d6b00f1502c240abaa5ad8d7118ed4538ccb2e
brandiw <brandimichelle.0911@gmail.com>
2017-05-16 17:03:25 -0700

    Adding a new branch for Alerts

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -40,26 +40,54 @@ angular.module('RecipeCtrls', ['RecipeServices'])
     });
   };
 }])
-.controller('NavCtrl', ['$scope', function($scope) {
+.controller('NavCtrl', ['$scope', 'Auth', function($scope, Auth) {
+  $scope.isLoggedIn  = function() {
+    return Auth.isLoggedIn();
+  }
+
   $scope.logout = function() {
-    // to implement
+    Auth.removeToken();
   };
 }])
-.controller('SignupCtrl', ['$scope', function($scope) {
+.controller('SignupCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
   $scope.user = {
     email: '',
     password: ''
   };
   $scope.userSignup = function() {
-    // to implement
+    $http.post('/api/users', $scope.user).then(function success(res){
+      console.log('successfully created a new user', res);
+      $location.path('/'); //relocate to the home page
+    }, function error(res){
+      console.log('Error while signing up', res);
+    });
   };
 }])
-.controller('LoginCtrl', ['$scope', function($scope) {
+.controller('LoginCtrl', ['$scope', '$timeout', 'Auth', '$http', '$location', 'Alerts', function($scope, $timeout, Auth, $http, $location, Alerts) {
   $scope.user = {
     email: '',
     password: ''
   };
+  var clearAlerts = function(){
+    Alerts.clear();
+  }
+
   $scope.userLogin = function() {
-    // to implement
+    $http.post('/api/auth', $scope.user).then(function success(res){
+      console.log('response from server when loggin in:', res);
+      Auth.saveToken(res.data.token);
+      Alerts.add('success', 'You are now logged in, congrats.');
+      $timeout(clearAlerts, 1500);
+      $location.path('/'); //redirect to home
+    }, function error(res){
+      console.log('Something went wrong', res);
+      Alerts.add('error', 'Bad Login Info, Please Try Again!!');
+      $timeout(clearAlerts, 1500);
+    });
   };
+}])
+.controller('AlertsController', ['$scope', 'Alerts', function($scope, Alerts){
+  $scope.alerts = function(){
+    return Alerts.get();
+  }
 }]);
